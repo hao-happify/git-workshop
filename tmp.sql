32b0f8b2f41f6741fd1cad3e0b7434cf397dee2a
Chia-Hao Shen <chia-hao@happify.com>
2023-05-04 11:54:00 -0400

    update sql file

--- tmp.sql
+++ tmp.sql
@@ -1,1 +1,8 @@
-SELECT * FROM EDW
+USE B2C;
+SELECT DISTINCT TOP 10
+    client_input,
+    dialog_prompt,
+    turn_number,
+    (conversation_id)
+FROM dialogs_log
+WHERE dialog_id = 983 AND dialog_prompt LIKE '%two more %' AND client_input LIKE '%Yes%'
